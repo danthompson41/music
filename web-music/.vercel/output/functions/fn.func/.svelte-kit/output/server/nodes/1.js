72faae240fa6378f4dd5687798286b97fbe7a6b9
Dan Thompson <danthompson41@gmail.com>
2023-12-27 13:01:29 -0500

    Removing vercel folder

--- web-music/.vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.i1qip0xw.js","_app/immutable/chunks/scheduler.1_433SNf.js","_app/immutable/chunks/index.YR9ItpMM.js","_app/immutable/chunks/singletons.HqvTyMBB.js","_app/immutable/chunks/index.BomQGc-Q.js"];
-export const stylesheets = [];
-export const fonts = [];
